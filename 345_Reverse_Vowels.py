76eb1cc04668d606da8744fc49715bcf65c11924
wangbiao92 <516947892@qq.com>
2017-01-14 15:43:33 +0800

    元音反转

---
+++ 345_Reverse_Vowels.py
@@ -0,0 +1,19 @@
+def reverseVowels(self, s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    vowels = ["a", "o", "e", "i", "u", "A", "O", "E", "I", "U"]
+    i = 0
+    j = len(s)
+    l = list(s)//字符串转成列表
+    while  i < j-1:
+        if l[i] in vowels and l[j - 1] in vowels:
+            l[i], l[j - 1] = l[j - 1], l[i]
+            i += 1
+            j -= 1
+        if l[i] not in vowels:
+            i += 1
+        if l[j-1] not in vowels:
+            j -= 1
+    return "".join(l)
